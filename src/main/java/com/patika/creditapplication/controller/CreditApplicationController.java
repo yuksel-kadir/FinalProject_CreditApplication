73f9b1d0b3ba7d039ea1bde2b858fac36fe78860
WastedHymn <kadir.yuksel@outlook.com.tr>
2023-02-16 23:00:03 +0300

    Add client registration function to register a client and make validation.

--- src/main/java/com/patika/creditapplication/controller/CreditApplicationController.java
+++ src/main/java/com/patika/creditapplication/controller/CreditApplicationController.java
@@ -1,16 +1,26 @@
 package com.patika.creditapplication.controller;
 
+import com.patika.creditapplication.dto.NewClient;
+import com.patika.creditapplication.service.ClientService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/creditApplication")
+@AllArgsConstructor
 public class CreditApplicationController {
+    private final ClientService clientService;
+    //@GetMapping("/applicationQuery")
 
-    @GetMapping("/registration")
-    public ResponseEntity<String> naturalEntityRegistration() {
-        return null;
+    //localhost:8080/creditApplication/registration
+    @PostMapping("/application")
+    public ResponseEntity<NewClient> clientRegistration(@Valid @RequestBody NewClient newClient) {
+        System.out.println("New Client:" + newClient);
+        clientService.RegisterClient(newClient);
+        return new ResponseEntity<>(newClient, HttpStatus.OK);
     }
 }
