6952c9da5e162aae0d11bcad8f8ebb25dc09ae7f
WastedHymn <kadir.yuksel@outlook.com.tr>
2023-02-21 03:23:28 +0300

    Add new function to get the credit limit and the credit status.

--- src/main/java/com/patika/creditapplication/service/strategy/StrategyContext.java
+++ src/main/java/com/patika/creditapplication/service/strategy/StrategyContext.java
@@ -1,5 +1,8 @@
 package com.patika.creditapplication.service.strategy;
 
+import com.patika.creditapplication.dto.CreditStrategyResult;
+import com.patika.creditapplication.dto.NewClient;
+import com.patika.creditapplication.exception.CreditStrategyNotFoundException;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -10,5 +13,24 @@ import java.util.List;
 public class StrategyContext {
     private final List<CreditStrategy> creditStrategyList;
 
+    public CreditStrategyResult getCreditStrategyResult(NewClient newClient, Integer clientCreditScore) {
+        //Find the appropriate strategy
+        CreditStrategy suitableCreditStrategy = creditStrategyList.stream()
+                .filter(creditStrategy -> creditStrategy.isSuitableStrategy(
+                        clientCreditScore,
+                        newClient.getMonthlyIncome())
+                )
+                .findFirst()
+                .orElseThrow(CreditStrategyNotFoundException::new);
 
+        System.out.println("Credit Strategy Class: " + suitableCreditStrategy.getClass().toString());
+        //Return a strategy result.
+        return new CreditStrategyResult(
+                suitableCreditStrategy.getCreditStatus(),
+                suitableCreditStrategy.calculateCreditLimit(
+                        newClient.getMonthlyIncome(),
+                        newClient.getCollateral()
+                )
+        );
+    }
 }
