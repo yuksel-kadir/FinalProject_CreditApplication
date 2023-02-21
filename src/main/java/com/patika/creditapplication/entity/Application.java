4284f7c9b45b7358e80580b7f02c5f392051dfa7
WastedHymn <kadir.yuksel@outlook.com.tr>
2023-02-21 03:02:32 +0300

    Map credit application table to the Application class.

--- src/main/java/com/patika/creditapplication/entity/Application.java
+++ src/main/java/com/patika/creditapplication/entity/Application.java
@@ -1,5 +1,31 @@
 package com.patika.creditapplication.entity;
 
 
+import com.patika.creditapplication.enums.CreditStatus;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "credit_applications")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Application {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "credit_limit")
+    private Float creditLimit;
+
+    private Float collateral;
+
+    @Column(name = "is_approved")
+    @Enumerated
+    private CreditStatus isApproved;
 }
