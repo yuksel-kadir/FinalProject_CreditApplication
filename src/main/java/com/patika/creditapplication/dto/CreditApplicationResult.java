9315fbdbb716412abcf28026cca81a8b7a9a6425
WastedHymn <kadir.yuksel@outlook.com.tr>
2023-02-21 03:20:36 +0300

    Rename class and add credit score field.

--- src/main/java/com/patika/creditapplication/dto/CreditApplicationResult.java
+++ src/main/java/com/patika/creditapplication/dto/CreditApplicationResult.java
@@ -1,13 +1,18 @@
 package com.patika.creditapplication.dto;
 
 import com.patika.creditapplication.enums.CreditStatus;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
-public class ClientApplicationResult {
+@AllArgsConstructor
+@Builder
+public class CreditApplicationResult {
     private String firstName;
     private String lastName;
     private String identityNumber;
     private Float creditLimit;
+    private Integer creditScore;
     private CreditStatus creditStatus;
 }
