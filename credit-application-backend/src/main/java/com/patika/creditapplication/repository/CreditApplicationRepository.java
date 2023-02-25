81bf02d8db850c1c253c47111bf30d65e3b8742c
WastedHymn <kadir.yuksel@outlook.com.tr>
2023-02-26 02:08:56 +0300

    Update import.

--- credit-application-backend/src/main/java/com/patika/creditapplication/repository/CreditApplicationRepository.java
+++ credit-application-backend/src/main/java/com/patika/creditapplication/repository/CreditApplicationRepository.java
@@ -1,8 +1,8 @@
 package com.patika.creditapplication.repository;
 
-import com.patika.creditapplication.entity.Application;
+import com.patika.creditapplication.entity.CreditApplication;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CreditApplicationRepository extends JpaRepository<Application, Long> {
+public interface CreditApplicationRepository extends JpaRepository<CreditApplication, Long> {
 
 }
