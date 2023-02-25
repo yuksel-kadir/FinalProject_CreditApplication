d96d7ff4fe564fcc2129e062dbe66f18d042493f
WastedHymn <kadir.yuksel@outlook.com.tr>
2023-02-26 02:08:38 +0300

    Add credit result util.

---
+++ credit-application-backend/src/main/java/com/patika/creditapplication/util/CreditResultUtil.java
@@ -0,0 +1,10 @@
+package com.patika.creditapplication.util;
+
+public final class CreditResultUtil {
+    private CreditResultUtil() {
+        throw new UnsupportedOperationException("This is a constant class and cannot be instantiated");
+    }
+
+    public static final String APPROVED = "Approved";
+    public static final String REJECTED = "Rejected";
+}
