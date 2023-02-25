4ba5517d11ed50a5679775a7d6aaf7875755c8fa
WastedHymn <kadir.yuksel@outlook.com.tr>
2023-02-26 02:38:22 +0300

    Add credit application api.

---
+++ credit-application-client/src/api/creditApplicationApi.js
@@ -0,0 +1,38 @@
+import axios from "axios";
+
+export const postCreditApplication = (body) => {
+  return axios.post("http://localhost:8080/client/creditApplication", body);
+};
+
+export const getCreditApplication = (body) => {
+  console.log("getCreditApplication request body-> ");
+  console.log(body);
+  return axios.post("http://localhost:8080/client/findCreditApplication", body);
+};
+
+export const updateLastName = (body) => {
+  console.log("updateLastName request body-> ");
+  console.log(body);
+  return axios.post("http://localhost:8080/client/updateClientInformation/lastName", body);
+};
+
+export const updateFirstName = (body) => {
+  console.log("updateFirstName request body-> ");
+  console.log(body);
+  return axios.post(
+    "http://localhost:8080/client/updateClientInformation/firstName", body);
+};
+
+export const updatePhoneNumber = (body) => {
+  console.log("updatePhoneNumber request body");
+  console.log(body);
+  return axios.post(
+    "http://localhost:8080/client/updateClientInformation/phoneNumber", body);
+}
+
+export const updateCreditApplication = (body) => {
+  console.log("updateCreditApplication request body");
+  console.log(body);
+  return axios.post(
+    "http://localhost:8080/creditApplication/updateCreditApplication", body);
+};
