316ccdde734bc7e3036bb5e4eaef4d902c91f113
WastedHymn <kadir.yuksel@outlook.com.tr>
2023-02-26 02:20:22 +0300

    Add client information updater functions. Add services and sms sender client. Add function to register new credit application.

--- credit-application-backend/src/main/java/com/patika/creditapplication/service/ClientService.java
+++ credit-application-backend/src/main/java/com/patika/creditapplication/service/ClientService.java
@@ -1,8 +1,15 @@
 package com.patika.creditapplication.service;
 
-import com.patika.creditapplication.dto.NewClient;
-import com.patika.creditapplication.entity.Application;
+import com.patika.creditapplication.smssystem.SMSSenderClient;
+import com.patika.creditapplication.dto.ClientInformation;
+import com.patika.creditapplication.dto.CreditStrategyResult;
+import com.patika.creditapplication.dto.request.ClientUpdateNameParam;
+import com.patika.creditapplication.dto.request.ClientUpdatePhoneNumberParam;
+import com.patika.creditapplication.dto.request.NewClient;
+import com.patika.creditapplication.dto.response.CreditApplicationResult;
 import com.patika.creditapplication.entity.Client;
+import com.patika.creditapplication.entity.CreditApplication;
+import com.patika.creditapplication.enums.Carrier;
 import com.patika.creditapplication.exception.ClientExistsException;
 import com.patika.creditapplication.exception.ClientNotFoundException;
 import com.patika.creditapplication.repository.ClientRepository;
@@ -18,13 +25,13 @@ import java.time.LocalDate;
 @Slf4j
 public class ClientService {
     private final ClientRepository clientRepository;
+    private final CreditScoreService creditScoreService;
+    private final ApplicationService applicationService;
+    private final SMSSenderClient smsSenderClient;
 
-    //NewClient newClient, Integer clientCreditScore, Application newApplication
     @Transactional
     public Client addClient(Client newClient) {
-        Client client = findClientByIdentityOrPhoneNumber(
-                newClient.getIdentityNumber(),
-                newClient.getPhoneNumber());
+        Client client = findClientByIdentityOrPhoneNumber(newClient.getIdentityNumber(), newClient.getPhoneNumber());
         if (client == null) {
             log.info("Adding new client: {}", newClient);
             return clientRepository.save(newClient);
@@ -32,7 +39,25 @@ public class ClientService {
         throw new ClientExistsException();
     }
 
-    public Client getClientObject(NewClient newClient, Integer creditScore, Application creditApplication){
+    public CreditApplicationResult processCreditApplication(NewClient newClient) {
+        log.info("Processing credit application for: {}", newClient);
+        Integer creditScore = creditScoreService.getCreditScore();
+        CreditStrategyResult strategyResult = applicationService.getStrategyResult(newClient, creditScore);
+
+        CreditApplication newCreditApplication = applicationService.getApplicationObjectForNewClient(strategyResult);
+        Client clientToAdd = getClientObject(newClient, creditScore, newCreditApplication);
+        Client client = addClient(clientToAdd);
+        log.info("New client: {} added to the database.", client);
+
+        CreditApplicationResult applicationResult = applicationService.getCreditApplicationResult(newClient, strategyResult);
+        String smsMessage = "Your credit application result -> Credit Limit: " + applicationResult.getCreditLimit() +
+                " Credit Status: " + applicationResult.getCreditStatus();
+        smsSenderClient.sendSMS(Carrier.TURKCELL, smsMessage, client.getPhoneNumber());
+        return applicationResult;
+    }
+
+
+    public Client getClientObject(NewClient newClient, Integer creditScore, CreditApplication creditApplication) {
         return Client.builder()
                 .firstName(newClient.getFirstName())
                 .lastName(newClient.getLastName())
@@ -41,15 +66,16 @@ public class ClientService {
                 .dateOfBirth(newClient.getDateOfBirth())
                 .monthlyIncome(newClient.getMonthlyIncome())
                 .creditScore(creditScore)
-                .application(creditApplication)
+                .creditApplication(creditApplication)
                 .build();
     }
+
     @Transactional
     public void deleteClient(String identityNumber) {
         Client client = clientRepository.findClientByIdentityNumber(identityNumber);
         if (client == null)
             throw new ClientNotFoundException();
-        System.out.println("Deleting: " + client);
+        log.info("Deleting client: {}", client);
         clientRepository.delete(client);
     }
 
@@ -59,13 +85,59 @@ public class ClientService {
         );
     }
 
-    public Client findClientByIdentityOrPhoneNumber(String identity, String phoneNumber){
+    public Client findClientByIdentityOrPhoneNumber(String identity, String phoneNumber) {
         return clientRepository.findClientByIdentityNumberOrPhoneNumber(identity, phoneNumber);
     }
-    /*
-    public boolean doesClientExist(String identity, String phoneNumber){
-        Client client = findClientByIdentityOrPhoneNumber(identity, phoneNumber);
-        return client != null;
+
+    public CreditApplicationResult findApplicationByIdentityNumberAndDateOfBirth(String identity, LocalDate dob) {
+        Client client = findClientByIdentityNumberAndBirthDate(identity, dob);
+        if (client == null)
+            throw new ClientNotFoundException();
+        return applicationService.getCreditApplicationResult(client);
     }
-    */
+
+    @Transactional
+    public String updateClientFirstName(ClientUpdateNameParam params){
+        Client client = clientRepository.findClientByIdentityNumber(params.getIdentityNumber());
+        if(client == null)
+            throw new ClientNotFoundException();
+        client.setFirstName(params.getInput());
+        Client changedClient = clientRepository.save(client);
+        log.info("Changed client name from {} to {}", params.getInput(), client.getFirstName());
+        return changedClient.getFirstName() + " " + changedClient.getLastName();
+    }
+
+    @Transactional
+    public String updateClientLastName(ClientUpdateNameParam params){
+        Client client = clientRepository.findClientByIdentityNumber(params.getIdentityNumber());
+        if(client == null)
+            throw new ClientNotFoundException();
+        client.setLastName(params.getInput());
+        Client changedClient = clientRepository.save(client);
+        log.info("Changed client last name from {} to {}", params.getInput(), client.getLastName());
+        return changedClient.getFirstName() + " " + changedClient.getLastName();
+    }
+
+    @Transactional
+    public String updateClientPhoneNumber(ClientUpdatePhoneNumberParam params){
+        Client client = clientRepository.findClientByIdentityNumber(params.getIdentityNumber());
+        if(client == null)
+            throw new ClientNotFoundException();
+        client.setPhoneNumber(params.getPhoneNumber());
+        Client changedClient = clientRepository.save(client);
+        log.info("Changed client phone number from {} to {}", params.getPhoneNumber(), changedClient.getPhoneNumber());
+        return changedClient.getPhoneNumber();
+    }
+
+    public ClientInformation getClientInformation(String identityNumber){
+        Client client = clientRepository.findClientByIdentityNumber(identityNumber);
+        if(client == null)
+            throw new ClientNotFoundException();
+        return ClientInformation.builder()
+                .firstName(client.getFirstName())
+                .lastName(client.getLastName())
+                .creditApplicationId(client.getCreditApplication().getId())
+                .creditScore(client.getCreditScore())
+                .build();
+    }
 }
