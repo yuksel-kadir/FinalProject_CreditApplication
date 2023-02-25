760848a61eeec3afecca57b67d20301fcd84e387
WastedHymn <kadir.yuksel@outlook.com.tr>
2023-02-26 02:18:39 +0300

    Delete "credit registration" and "credit application search" endpoints. Add "update credit application" endpoint.

--- credit-application-backend/src/main/java/com/patika/creditapplication/controller/CreditApplicationController.java
+++ credit-application-backend/src/main/java/com/patika/creditapplication/controller/CreditApplicationController.java
@@ -1,11 +1,12 @@
 package com.patika.creditapplication.controller;
 
-import com.patika.creditapplication.dto.CreditApplicationQuery;
-import com.patika.creditapplication.dto.CreditApplicationResult;
-import com.patika.creditapplication.dto.NewClient;
+import com.patika.creditapplication.dto.ClientInformation;
+import com.patika.creditapplication.dto.request.CreditApplicationUpdateParams;
+import com.patika.creditapplication.dto.response.CreditApplicationUpdateResult;
 import com.patika.creditapplication.response.Response;
 import com.patika.creditapplication.response.ResponseData;
 import com.patika.creditapplication.service.ApplicationService;
+import com.patika.creditapplication.service.ClientService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,26 +17,26 @@ import javax.validation.Valid;
 @RestController
 @RequestMapping("/creditApplication")
 @AllArgsConstructor
+@CrossOrigin
 public class CreditApplicationController {
     private final ApplicationService applicationService;
-
+    private final ClientService clientService;
     //localhost:8080/creditApplication/findApplication
-    @GetMapping("/findApplication")
-    public ResponseEntity<Response> findApplication(@Valid @RequestBody CreditApplicationQuery query) {
-        CreditApplicationResult result = applicationService.findApplicationByIdentityNumberAndDateOfBirth(query.getIdentityNumber(), query.getDateOfBirth());
-        return new ResponseEntity<>(
-                new ResponseData(200, "Query processed successfully.", result),
-                HttpStatus.FOUND
-        );
-    }
 
-    //localhost:8080/creditApplication/registration
-    @PostMapping("/application")
-    public ResponseEntity<Response> creditApplicationRegistration(@Valid @RequestBody NewClient newClient) {
-        CreditApplicationResult applicationResponse = applicationService.processCreditApplication(newClient);
+    @PostMapping("/updateCreditApplication")
+    public ResponseEntity<Response> updateCreditApplication(@Valid @RequestBody CreditApplicationUpdateParams params){
+        ClientInformation clientInformation = clientService.getClientInformation(params.getIdentityNumber());
+        clientInformation.setNewMonthlyIncome(params.getMonthlyIncome());
+        clientInformation.setNewCollateral(params.getCollateral());
+        CreditApplicationUpdateResult updateResult = applicationService.updateCreditApplication(clientInformation);
         return new ResponseEntity<>(
-                new ResponseData(200, "Credit application registration is complete.", applicationResponse),
+                new ResponseData(
+                        200,
+                        "Credit Application is updated.",
+                        updateResult
+                ),
                 HttpStatus.OK
         );
     }
+
 }
