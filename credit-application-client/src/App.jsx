58b1b4d603567ae72cb697f28036de56dc79cf95
WastedHymn <kadir.yuksel@outlook.com.tr>
2023-02-27 01:07:18 +0300

    Add delete client page to the routes.

--- credit-application-client/src/App.jsx
+++ credit-application-client/src/App.jsx
@@ -2,21 +2,22 @@ import "./App.css";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Homepage from "./pages/creditApplicationPage";
 import NavigationBar from "./components/NavigationBar";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import "react-datepicker/dist/react-datepicker.css";
 import CheckCreditApplicationPage from "./pages/checkCreditApplicationPage";
 import UpdateInformationPage from "./pages/updateInformationPage";
-
+import DeleteClientPage from "./pages/deleteClientPage";
 
 function App() {
   return (
     <BrowserRouter>
-      <NavigationBar/>
+      <NavigationBar />
       <Routes>
-        <Route path="/" element={<Navigate to="/creditApplication" />}/>
-        <Route path="/creditApplication" element={<Homepage/>}/>
-        <Route path="/updateInformations" element={<UpdateInformationPage/>}/>
-        <Route path="/checkCreditApplication" element={<CheckCreditApplicationPage/>}/>   
+        <Route path="/" element={<Navigate to="/creditApplication" />} />
+        <Route path="/creditApplication" element={<Homepage />} />
+        <Route path="/updateInformations" element={<UpdateInformationPage />} />
+        <Route path="/checkCreditApplication" element={<CheckCreditApplicationPage />}/>
+        <Route path="/deleteClient" element={<DeleteClientPage />} />
       </Routes>
     </BrowserRouter>
   );
